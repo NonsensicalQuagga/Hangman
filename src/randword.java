bcb5c8fc9049bff61f2d05f1c8fbea2c354ac7bb
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2022-12-13 14:36:36 +0100

    added files

---
+++ src/randword.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class randword {
+    public static String randword(int x) throws FileNotFoundException {
+    Scanner in = new Scanner(new File("Ord.txt"));
+
+    }
+
+}
