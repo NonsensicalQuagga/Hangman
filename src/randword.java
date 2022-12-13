7bad0ebeca945549a726cbc04df512e61f781101
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2022-12-13 15:07:24 +0100

    might do something

--- src/randword.java
+++ src/randword.java
@@ -3,12 +3,18 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class randword {
-    public static String randword(int x) throws FileNotFoundException {
-    Scanner in = new Scanner(new File("Ord.txt"));
+    public static String randword(int x){
+     try {
+        String returns = "a";
+
+            Scanner in = new Scanner(new File("Ord.txt"));
+
+
     for(int i = 0 ; i < x ; i++){
-
+    returns = in.nextLine();
     }
-
+    return returns;
+     }catch (FileNotFoundException e){return "hej";}
     }
 
 }
