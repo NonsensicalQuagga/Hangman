8f606a4496ac4f5a813abd726de9cbdaed0f7fab
massiv1984 <94972814+massiv1984@users.noreply.github.com>
2022-12-16 10:58:56 +0100

    to not get conflicting files

--- src/Hangman.java
+++ src/Hangman.java
@@ -7,9 +7,9 @@ public class Hangman {
         int min = 1;
         int range = max - min + 1;
 
-        String ordet = randword.randomword((int)(Math.random() * range) + min).toUpperCase();
-        while  (ordet.length()<2) ordet = randword.randomword((int)(Math.random() * range) + min).toUpperCase();
-        System.out.println(ordet);
+    String ordet = randword.randomword((int)(Math.random() * range) + min).toUpperCase();
+    while  (ordet.length()<2) ordet = randword.randomword((int)(Math.random() * range) + min).toUpperCase();
+    System.out.println(ordet);
 
     }
 }
