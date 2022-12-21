41f7c0eeaa6d01578636b5cc2d6232fe32833755
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2022-12-21 10:56:31 +0100

    Added wrong guess logic and fixed word comprehension

--- src/Hangman.java
+++ src/Hangman.java
@@ -13,6 +13,8 @@ public class Hangman {
         boolean legalWord;
         String guessedLetters = "";
         String guessedWords = "";
+        int wrongGuessCounter = 0;
+        int illegalGuessCounter= 0;
 
         String theWord = randomWord.u((int)(Math.random() * range) + min).toUpperCase();
         while  (theWord.length()<2) theWord = randomWord.u((int)(Math.random() * range) + min).toUpperCase();
@@ -20,20 +22,24 @@ public class Hangman {
 
         String underscore = underscoreCreate.u(theWord);
 
-        while (!Objects.equals(underscore, theWord)) {
+        while (!Objects.equals(underscore, theWord) && wrongGuessCounter != 11) {
             System.out.println(underscore);
+
             guess = in.nextLine().toUpperCase();
             legalWord = legalGuess.u(guess, theWord, guessedLetters, guessedWords);
             if (legalWord){
                 String blank = goodGuess.u(theWord, guess, underscore);
-
-            // add more logic with underscore and isTrue
-            underscore = blank;
+                if (underscore.equals(blank)) wrongGuessCounter++;
+                if (!Objects.equals(underscore,blank)) underscore = blank;
             }
-
+            System.out.println(wrongGuessCounter);
             if (!guessedLetters.contains(guess) && guess.length() == 1 && legalWord) guessedLetters = guessedLetters + guess;
             if (!guessedWords.contains(guess) && !Objects.equals(guess, theWord) && guess.length() == theWord.length() && legalWord) guessedWords = guessedWords + " " + guess;
-
+            if (!legalWord) illegalGuessCounter++;
+            if (illegalGuessCounter == 3) {
+                illegalGuessCounter = 0;
+                wrongGuessCounter++;
+            }
         }
     }
 }
--- src/goodGuess.java
+++ src/goodGuess.java
@@ -3,11 +3,15 @@ public class goodGuess {
 
         String blank = "";
         String imBad = "_";
-        for (int i = 0  ; i < theWord.length(); i++){
-            if (theWord.charAt(i) == guess.charAt(0))blank = blank + guess;
-            if (theWord.charAt(i) != guess.charAt(0) && underscore.charAt(i) ==imBad.charAt(0)) blank = blank + "_";
-            if (theWord.charAt(i) != guess.charAt(0) && underscore.charAt(i) != imBad.charAt(0))blank = blank + underscore.charAt(i);
+        if (guess.length()==1){
+            for (int i = 0  ; i < theWord.length(); i++){
+                if (theWord.charAt(i) == guess.charAt(0))blank = blank + guess;
+                if (theWord.charAt(i) != guess.charAt(0) && underscore.charAt(i) == imBad.charAt(0)) blank = blank + "_";
+                if (theWord.charAt(i) != guess.charAt(0) && underscore.charAt(i) != imBad.charAt(0))blank = blank + underscore.charAt(i);
+            }
         }
+        if (guess.length() == theWord.length() && guess.equals(theWord)) blank = guess;
+        else blank = underscore;
         return blank;
     }
 }
