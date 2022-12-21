fdd1d86a3de643b418846c4a92c1052f101e2125
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2022-12-21 10:04:47 +0100

    Removed some empty lines

--- src/underscoreCreate.java
+++ src/underscoreCreate.java
@@ -1,17 +1,13 @@
 public class underscoreCreate {
     public static String u(String theWord) {
 
-
         String underscore = "";
         String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ".toUpperCase();
-
 
         for (int i = 0; i < theWord.length(); i++) {
             if (abc.contains(String.valueOf(theWord.charAt(i)))) underscore = underscore + "_";
             else underscore = underscore + theWord.charAt(i);
         }
-
-
         return underscore;
     }
 }
