608daec4aaf9979b1fe0aec445a4d5390467a1ef
massiv1984 <94972814+massiv1984@users.noreply.github.com>
2023-01-13 11:04:50 +0100

    hedde ascii i koden

--- src/hangdgubbe.java
+++ src/hangdgubbe.java
@@ -1,12 +1,137 @@
+import java.util.Arrays;
+
 public class hangdgubbe {
-    public static void main(String[] args) {
+    public static boolean art(int antalFel){
         // gör en funktion som tar input 1 - 11 och ger ut en "bild"
 
 
 
+if (antalFel == 1){
+    System.out.println("       _________");
+    System.out.println("      /         \\");
+    System.out.println("     /           \\");
+}
 
+        if (antalFel == 2){
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
 
+        if (antalFel == 3){
+            System.out.println("           ,_________");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
 
+        if (antalFel == 4){
+            System.out.println("           ,_________");
+            System.out.println("           | /");
+            System.out.println("           |/");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 5){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 6){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/       O");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 7){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/       O");
+            System.out.println("           |        |");
+            System.out.println("           |");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 8){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/       O");
+            System.out.println("           |        |");
+            System.out.println("           |       /");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 9){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/       O");
+            System.out.println("           |        |");
+            System.out.println("           |       / \\");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 10){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/      \\O");
+            System.out.println("           |        |");
+            System.out.println("           |       / \\");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+        if (antalFel == 11){
+            System.out.println("           ,_________");
+            System.out.println("           | /      |");
+            System.out.println("           |/      \\O/");
+            System.out.println("           |        |");
+            System.out.println("           |       / \\");
+            System.out.println("           |");
+            System.out.println("       ____A____");
+            System.out.println("      /         \\");
+            System.out.println("     /           \\");
+        }
+
+    return true;
 
     }
 }
